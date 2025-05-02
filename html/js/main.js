850bb7f318e93a4f8a4c4c59c760298e6e7dabdd
anagabriela <anagarcia.comunicacion@gmail.com>
2025-05-02 09:06:15 -0600

    Se sube descripción de las tarjetas

--- html/js/main.js
+++ html/js/main.js
@@ -1,49 +1,48 @@
+document.addEventListener('DOMContentLoaded', function(){
+
 const main = document.getElementsByTagName("main").item(0);
-let tarjetas = document.getElementById("tarjetas");
+const tarjetas = document.querySelectorAll(".tarjetas .col");
 const botonClic = document.getElementById ('botonClic');
 const URLMain="https://api.escuelajs.co/api/v1/products/";
 
-botonClic.addEventListener('click', function(event){
-    event.preventDefault();
 
-    function getData(information){
-        const options = {method: "GET"};
-        fetch(URLMain, options)
-            .then((response) => {
-                response.json().then((res)=>{
-                    //console.log(res.length);
-                    //console.log(res[2].title);
-                    createCard(res)
-            });
-        })
+botonClic.addEventListener('click', function(){
+    fetch(URLMain)
+    .then(response => response.json())
+    .then(data => {
+        tarjetas.forEach((tarjeta, i)=> {
+            const producto = data [i];
+            console.log("Producto:", producto);
+
+            if(producto){
+                const svg = tarjeta.querySelector (".bd-placeholder-img");
+
+                //En Go Live me mostraba imágenes rotas, así que busqué cuál podría ser el motivo. 
+                // Encontré que era mejor tratar al svg como img y así  poder llamar al url. Lo intenté pero sigo tenieendo las imágenes "rotas" en Go live.
+                //Seguiré investigando.
+                if(svg){
+                const img = document.createElement("img");
+                img.classList.add("card-img-top");
+                img.src = producto.images [1];
+                svg.replaceWith(img);
+                
+            }
+            }
+
+            tarjeta.querySelector(".card-text").innerText= producto.description;
+            tarjeta.querySelector(".price").innerText= `${"Precio: " + producto.price +" pesos"}`;
+              
+        });
+    })
     
             .catch((err) => {
                          main.insertAdjacentHTML("beforeend", 
                              `<div class="alert alert-danger" role="alert">
                                   ${err.message}
                              </div>`);
-                        
+             });
+                   
        });
-    
-    }//getData
-
-    function createCard(productos){
-        tarjetas.innerText= "";
-        productos.forEach(producto => {
-        tarjetas.insertAdjacentHTML("beforeend",
-            `<div class="card"  style="width: 18rem;">
-                <img src="${producto.images[0]}" class="card-img-top" alt="${producto.title}">
-                <div class="card-body">
-                    <h5 class="card-title">${producto.title}</h5>
-                    <p class="card-text">${producto.description}</p>
-                    <a href="#" class="btn btn-primary">Ver más</a>
-                </div>
-            </div>`
-        );
-    
     });
-    }
 
-    getData ();
-
-});
+    
